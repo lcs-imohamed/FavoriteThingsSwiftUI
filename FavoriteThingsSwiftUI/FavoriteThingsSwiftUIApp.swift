cb1a1e2e54e189f861e0a4e36d3ff75c6a6e6879
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-08 14:29:30 -0500

    Initial Commit

---
+++ FavoriteThingsSwiftUI/FavoriteThingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsSwiftUIApp.swift
+//  FavoriteThingsSwiftUI
+//
+//  Created by Mohamed, Ilana on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
